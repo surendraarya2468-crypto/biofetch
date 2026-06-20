1febd8c98cc36adee660d66a8fbe837253b27b85
surendraarya2468-crypto <surendraarya2468@gmail.com>
2026-06-20 23:55:36 +0530

    Implement handler for bioinformatics AI assistant

--- chat.js
+++ chat.js
@@ -1,1 +1,71 @@
+export default async function handler(req,res){
 
+const {question}=req.body;
+
+const response = await fetch(
+"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key="+process.env.GEMINI_API_KEY,
+{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+
+contents:[
+{
+parts:[
+{
+text:`
+
+You are BioFetch AI Assistant.
+
+Answer only bioinformatics and biotechnology questions.
+
+Topics:
+
+- Proteins
+- Genes
+- Genomics
+- Proteomics
+- UniProt
+- AlphaFold
+- PDB structures
+- Molecular biology
+- Microbiology
+- Environmental biotechnology
+- Biomining
+- Acid mine drainage
+- Bioenergy
+- Microbial remediation
+
+Explain scientifically and clearly.
+
+Question:
+
+${question}
+
+`
+}
+]
+}
+]
+
+})
+
+});
+
+
+const data = await response.json();
+
+
+res.status(200).json({
+
+answer:data.candidates[0].content.parts[0].text
+
+});
+
+
+}
